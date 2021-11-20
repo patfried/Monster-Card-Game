c8df6300ab7ae87fb0ff37cc422c50b6f02ea6de
patfried <technofr3ak11@gmail.com>
2021-11-20 17:47:08 +0100

    Projektdateien hinzufügen.

---
+++ Monster Card Game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Monster_Card_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
